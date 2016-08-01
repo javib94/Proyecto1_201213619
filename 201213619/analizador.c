8c6f735ca334f543fae64af5485931b6cbd33a6a
javib94 <javier.barreda94@gmail.com>
2016-08-01 17:52:09 -0600

    files2

--- 201213619/analizador.c
+++ 201213619/analizador.c
@@ -1,1 +1,35 @@
 #include "analizador.h"
+#include <string.h>
+
+void analizar_cadena(char *cadena){
+    int estado = 0;
+    int index = 0;
+    int i = 0;
+    int cadena2;
+    while(cadena[index]!='\0'){
+        switch(estado){
+            case 0:
+                    switch(cadena[index]){
+                        default:
+                        break;
+                    }
+                    break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+        }
+    }
+}
--- 201213619/analizador.h
+++ 201213619/analizador.h
@@ -2,7 +2,13 @@
 #define ANALIZADOR_H_INCLUDED
 
 
+//tipos te token: comando=1, parametro=2, igual=3, cadena=4, direccion=5
+struct token{
+    char cadena[250];
+    int tipo;
+}token;
 
+void analizar_cadena(char* cadena);
 
 
 
